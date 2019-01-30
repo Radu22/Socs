588140393c0ee94ddd1b26ec98ddc2cbd28b4d76
Radu Maftei <Radu.Maftei@endava.com>
2019-01-30 15:21:19 +0200

    wqdsa

---
+++ scripts/json_io.py
@@ -0,0 +1,32 @@
+import twitter, json
+
+
+
+consumer_key = '9M4W5fdGcoJIAnbPFl3TbFXj6'
+consumer_secret = 'Q9TqVpG4GgsvZ98EanH2VANWYLKSvBhKsfQvITJJykTbXhuJKp'
+access_token = '908491442-KMeSNsIXyLtsDI9ARJfzXg0SjgZAtXnFxm6yD4gu'
+access_secret = 'nD2a6fKB0FUsBHj6gTHKq6ZvtoLCARreOrVMKguYXe7TQ'
+
+api = twitter.Api(consumer_key =consumer_key,
+                  consumer_secret =consumer_secret,
+                  access_token_key = access_token,
+                  access_token_secret=access_secret)
+
+searchBy = api.GetSearch("cats")
+idlist = list()
+for i in searchBy:
+    idlist.append(i.id)
+
+imagesList = list()
+for id in idlist:
+    if api.GetStatus(id).media is not None:
+        imagesList.append(api.GetStatus(id).media[0].media_url)
+
+with open("media_urls.json", "w") as write_file:
+    json.dump(imagesList, write_file)
+	
+print(imagesList)
+
+
+
+
---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+// import {fs} from 'fs';
+
+function getMeta(url){   
+    var img = new Image();
+    img.onload = function(){
+        console.log( this.width+' '+ this.height );
+    };
+    img.src = url;
+}
+
+function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'media_urls.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+ 
+ function init() {
+    loadJSON(function(response) {
+     // Parse JSON string into object
+       var actual_JSON = JSON.parse(response);
+       console.log(actual_JSON);
+    });
+   }
+
+   init()   
+    
+
+// readTextFile('media_urls.json');
+// getMeta('https://pbs.twimg.com/media/Dxzo9K3UwAAh08K.jpg');
